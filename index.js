14d7e6a6473ebbfb71be9dacde0e0a4986d48036
Julian Ceballos <julian@pikhub.com>
2014-06-10 17:45:22 -0500

    listen port changed

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ http.createServer(function(request, response) {
   response.writeHead(200);
   response.write('<h1>Hello world!</h1><p><strong>Environment:</strong> ' + process.env.NODE_ENV + '</p>');
   response.end();
-}).listen(8789);
+}).listen('8789');
