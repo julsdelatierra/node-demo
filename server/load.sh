538b96a9ffbd44d9b3928461eda3064b313c0a9a
Julian Ceballos <julian@pikhub.com>
2014-06-10 18:34:47 -0500

    updated

--- server/load.sh
+++ server/load.sh
@@ -8,8 +8,8 @@ NGINX_CONFIG_DIR=/etc/nginx/conf.d
 INIT_DIR=/etc/init.d
 
 # Symlink nginx and init scripts
-ln -s $WWW_DIR/server/node_demo.conf $NGINX_CONFIG_DIR/node_demo.conf
-ln -s $WWW_DIR/server/node_demo $INIT_DIR/node_demo
+ln -s $WWW_DIR/server/node-demo.conf $NGINX_CONFIG_DIR/node-demo.conf
+ln -s $WWW_DIR/server/node-demo $INIT_DIR/node-demo
 
 # Make dir for logs and pids
 mkdir $NGINX_LOG_DIR/$NAME
