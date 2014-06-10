21650e2814ed1bc0b1792fb8dd6dc0252ffb5c8d
Julian Ceballos <julian@pikhub.com>
2014-06-10 17:25:23 -0500

    demo project

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function(request, response) {
+  response.writeHead(200);
+  response.write('<h1>Hello world!</h1><p><strong>Environment:</strong> ' + process.env.NODE_ENV + '</p>');
+  response.end();
+}).listen(9997);
